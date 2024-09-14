6b4fd933cb91d506584ee893d052cbc75fb44177
Thanos Pappas <thanosdante@gmail.com>
2024-09-14 13:01:38 +0300

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 // next.config.mjs
 export default {
-  output: 'export', // Enables static export
+  output: 'export', // Enables static export for GitHub Pages
   basePath: '/menudemo', // Replace with your GitHub repository name
+  assetPrefix: '/menudemo/', // Ensures assets are correctly prefixed
   images: {
     unoptimized: true, // Disables image optimization for GitHub Pages
   },
+  trailingSlash: true, // Ensures all paths end with a slash, which is often needed for GitHub Pages
 };
