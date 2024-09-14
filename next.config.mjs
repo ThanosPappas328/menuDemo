d0cc76724ae8ba5a8b9a7f0f27d3397c81f8f53c
Thanos Pappas <thanosdante@gmail.com>
2024-09-14 14:51:57 +0300

    Update deployment workflow and remove next export command

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 // next.config.mjs
 export default {
-  output: 'export',  // Enables static export without running `next export`
+  output: 'export', // Enables static export
+  basePath: '/menudemo', // Set the base path to match the GitHub Pages subdirectory
+  assetPrefix: '/menudemo/', // Set the asset prefix to match the subdirectory
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
+  trailingSlash: true, // Ensures URLs end with a slash, which is useful for static sites
   webpack: (config) => {
     config.optimization.minimize = false;
     return config;
