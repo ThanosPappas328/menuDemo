98aa21f06a8b6b48177a0d9ddbdd47e45075ff80
ThanosPappas328 <ThanosPappas328@users.noreply.github.com>
2024-09-14 11:52:37 +0000

    deploy: d0cc76724ae8ba5a8b9a7f0f27d3397c81f8f53c

--- _next/static/chunks/app/page-47315a94963e261c.js
+++ _next/static/chunks/app/page-47315a94963e261c.js
@@ -1,6 +1,6 @@
 (self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([[931],{
 
-/***/ 3468:
+/***/ 8114:
 /***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
 
 Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 7372));
@@ -812,7 +812,7 @@ function IconBase(props) {
 },
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
-/******/ __webpack_require__.O(0, [613,699,648,971,23,744], function() { return __webpack_exec__(3468); });
+/******/ __webpack_require__.O(0, [613,699,648,971,23,744], function() { return __webpack_exec__(8114); });
 /******/ var __webpack_exports__ = __webpack_require__.O();
 /******/ _N_E = __webpack_exports__;
 /******/ }
