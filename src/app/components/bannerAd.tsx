a29cb43b52c7fba74a1292438409c44da9e2bd65
Thanos Pappas <thanosdante@gmail.com>
2024-09-14 15:25:52 +0300

    changes

--- src/app/components/bannerAd.tsx
+++ src/app/components/bannerAd.tsx
@@ -1,6 +1,7 @@
 // components/pages/punlishedMenu/bannerAd.tsx
 import React from 'react';
 import Image from 'next/image'; // Import Image from Next.js
+import styles from '../styles/bannerAd.module.css';
 
 interface BannerAdProps {
   imageUrl: string;
@@ -9,7 +10,7 @@ interface BannerAdProps {
 
 const BannerAd: React.FC<BannerAdProps> = ({ imageUrl, altText }) => {
   return (
-    <div className="banner-ad">
+    <div className={styles.bannerad}>
       {/* Use Next.js Image component for optimization */}
       <Image src={imageUrl} alt={altText} width={728} height={90} />
     </div>
